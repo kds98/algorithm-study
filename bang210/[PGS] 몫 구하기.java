faf82914bd35c8967933f2bc58091140063c4dea
Bang210 <95253075+Bang210@users.noreply.github.com>
2023-11-02 16:43:36 +0900

    강병우 / 6문제 (#11)
    * Create test
    
    * 테스트
    
    * [PGS] 두 수의 곱 / LV0
    
    * [PGS] 몫 구하기  / LV0
    
    * [PGS] 두 수의 차  / LV0
    
    * [PGS] 양꼬치  / LV0
    
    * [PGS] 편지  / LV0
    
    * [PGS] 피자 나눠먹기(1)  / LV0

---
+++ bang210/[PGS] 몫 구하기.java
@@ -0,0 +1,10 @@
+package com.ll;
+
+
+//num1을 num2로 나눈 몫을 구하는 문제
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
